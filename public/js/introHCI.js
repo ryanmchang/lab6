a9bea6c877d920f103fa706557eb318461bfb0ea
ryanmchang <ryanmchang@gmail.com>
2020-02-12 23:20:00 -0800

    add heroku

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -24,7 +24,7 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-	$.get("http://localhost:3000/project/" + idNumber, callBackFn);
+	$.get("https://lab6-rmc.herokuapp.com/project/" + idNumber, callBackFn);
 	console.log("User clicked on project " + idNumber);
 
 	function callBackFn (result) {
