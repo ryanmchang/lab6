fea1743e094305254cf64d3b876e30d250701c32
ryanmchang <ryanmchang@gmail.com>
2020-02-12 23:15:44 -0800

    first commit

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,7 +10,6 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$('.project a').click(addProjectDetails);
-
 	// $('#colorBtn').click(randomizeColors);
 }
 
@@ -25,6 +24,18 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
+	$.get("http://localhost:3000/project/" + idNumber, callBackFn);
 	console.log("User clicked on project " + idNumber);
+
+	function callBackFn (result) {
+		console.log("called back");
+		var projectHTML =
+		'<p>' + result['title'] + '</p>' +
+		'<p>' + result['date']  + '</p>' +
+    '<img src="' + result['image'] + '" class="detailsImage">' +
+    '<p>' + result['summary'] + '</p>';
+
+		//$("#project" + idNumber + " .details").html(result.summary);
+		$("#project" + idNumber + " .details").html(projectHTML);
+	}
 }
